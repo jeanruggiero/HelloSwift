a21a917c0550374afe983b2fbbeed440db06b866
Jean Ruggiero <jeanruggiero@gmail.com>
2019-12-12 15:59:19 -0500

    Initial Commit

---
+++ HelloSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloSwift
+//
+//  Created by Jean Ruggiero on 12/12/19.
+//  Copyright © 2019 Jean Ruggiero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
